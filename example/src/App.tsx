ec843beb4ecede8f997391033d9dd426e5f3707f
knowankit <44323532+knowankit@users.noreply.github.com>
2021-09-09 13:15:44 +0530

    feat: add carousel buttons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 import { ReactSmartCarousel } from 'react-smart-carousel'
 import 'react-smart-carousel/dist/index.css'
+import one from './images/1.jpg'
+import two from './images/2.jpg'
+import three from './images/3.jpg'
+import four from './images/4.jpg'
+import five from './images/5.jpg'
+import six from './images/6.jpg'
 
 const App = () => {
-  return <ReactSmartCarousel text='Create React Library Example 😄' />
+  return <ReactSmartCarousel images={[one, two, three, four, five, six]} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,86 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
 interface Props {
-  text: string
+  images: string[]
+  leftIcon?: JSX.Element
+  rightIcon?: JSX.Element
 }
 
-export const ReactSmartCarousel = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ReactSmartCarousel = ({ images }: Props) => {
+  const [filteredImagesRecord, setFilteredImagesRecord] = useState(images)
+  const [currentImage, setCurrentImage] = useState(images.length - 1)
+
+  const DEG = 3
+
+  const renderImages = (images: string[]) => {
+    const middle = Math.floor(images.length / 2)
+
+    return images.map((image, index) => (
+      <div
+        className={styles.imageDim}
+        key={index}
+        onClick={(e) => onImageClick(e)}
+        id={`smart-carousel-image-${index}`}
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: 'cover',
+          transform:
+            middle >= index
+              ? `rotate(${DEG + index * 3}deg)`
+              : `rotate(${DEG - index * 3}deg)`
+        }}
+      />
+    ))
+  }
+
+  const onImageClick = (e: any) => {
+    e.target.style.transform = `rotate(0deg)`
+  }
+
+  const updateImages = (image: HTMLElement | null) => {
+    setTimeout(() => {
+      const temp =
+        filteredImagesRecord.length === 0 ? images : filteredImagesRecord
+      setFilteredImagesRecord([...temp])
+      setCurrentImage(filteredImagesRecord.length - 1)
+      image?.remove()
+    }, 1000)
+
+    filteredImagesRecord.splice(-1, 1)
+  }
+
+  const onRightClick = () => {
+    const image = document.getElementById(
+      `smart-carousel-image-${currentImage}`
+    )
+
+    updateImages(image)
+
+    image?.classList.add(styles.slideOutRight)
+  }
+
+  const onLeftClick = () => {
+    const image = document.getElementById(
+      `smart-carousel-image-${currentImage}`
+    )
+
+    updateImages(image)
+
+    image?.classList.add(styles.slideOutLeft)
+  }
+
+  return (
+    <div className={styles.sliderContainer}>
+      <button className={styles.sliderButton} onClick={onLeftClick}>
+        {'<'}
+      </button>
+      <div className={styles.imageContainer}>
+        {renderImages(filteredImagesRecord)}
+      </div>
+      <button className={styles.sliderButton} onClick={onRightClick}>
+        {'>'}
+      </button>
+    </div>
+  )
 }
